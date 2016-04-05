3dc41a8cf5991a6e3929821d9310e5e9626debd7
dlaliberte <dlaliberte@tp1.ca>
2016-04-05 18:21:34 -0400

    tickets-255 add rubocop

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe 'charpentier' , :type => :aruba do
+RSpec.describe 'charpentier' , type: :aruba do
   before(:each) { run('jm') }
   it { expect(last_command_started).to be_successfully_executed }
 end
