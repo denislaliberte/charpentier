f26345741beced776e9487c3a4064959d579697c
dlaliberte <dlaliberte@tp1.ca>
2016-04-06 12:52:54 -0400

    add help text

--- spec/charpentier_spec.rb
+++ spec/charpentier_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe Charpentier, type: :aruba do
-  before(:each) { run('jm') }
-  it { expect(last_command_started).to be_successfully_executed }
-  it do
-    expect(last_command_started)
-      .to have_output an_output_string_matching 'Usage:'
+  describe 'jm' do
+    before(:each) { run('jm') }
+    it { expect(last_command_started).to be_successfully_executed }
+    it do
+      expect(last_command_started)
+        .to have_output an_output_string_matching 'Usage:'
+    end
+    it do
+      expect(last_command_started)
+        .to have_output an_output_string_matching '--help'
+    end
   end
-  it do
-    expect(last_command_started)
-      .to have_output an_output_string_matching '--help'
+  describe 'jm --help' do
+    before(:each) { run('jm --help') }
+    it do
+      help_message = '-h, --help .*Show command line help'
+      expect(last_command_started)
+        .to have_output an_output_string_matching help_message
+    end
   end
 end
