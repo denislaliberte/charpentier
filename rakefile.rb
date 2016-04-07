2cb2c6358e8aaa929e066f233a49cbdcec34ffd7
Denis Laliberté <denis@tp1.ca>
2016-04-07 19:57:26 -0400

    wip test publish to gh-page

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,16 @@
-desc "publish site : merge master to gh-pages and push to github"
+require 'git'
+require 'logger'
+
+desc 'publish site : merge master to gh-pages and push to github'
 task :publish do
-  puts "merge to gh-pages branch"
-  puts "push to github"
+  working_dir = File.dirname(__FILE__)
+  git = Git.open(working_dir, log: Logger.new(STDOUT))
+  git.checkout('gh-pages')
+  git.merge('master')
+  g.push(g.remote('origin'))
 end
 
-desc "list all task"
+desc 'list all task'
 task :default do
-  system("rake -T")
+  system('rake -T')
 end
-
