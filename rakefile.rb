807af0e21512f04ac15abd434010b768bbeb3208
Denis Laliberté <denis@tp1.ca>
2016-04-07 21:00:40 -0400

    WIP push to gh-pages

--- rakefile.rb
+++ rakefile.rb
@@ -1,15 +1,21 @@
 require 'git'
 require 'logger'
+require 'tempfile'
 
 desc 'publish site : merge master to gh-pages and push to github'
 task :publish do
+  puts 'rake publish'
   working_dir = File.dirname(__FILE__)
-  git = Git.open(working_dir, log: Logger.new(STDOUT))
-  current_branch = `git rev-parse --abbrev-ref HEAD`
+  log_path = working_dir + '/log/rake_pubish_' + Time.now.to_i.to_s + '.log'
+  file = File.new(log_path, 'w+')
+  git = Git.open(working_dir, log: Logger.new(file))
   git.checkout('gh-pages')
   git.merge('master')
   git.push(git.remote('origin'))
-  git.checkout(current_branch)
+  git.checkout('master')
+  file.close
+  puts 'log file'
+  puts file.path
 end
 
 desc 'list all task'
