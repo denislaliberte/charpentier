5745ad7fef10d6fdc13b03f0b00631af3dce8643
Denis Laliberté <denis@tp1.ca>
2016-04-07 20:00:08 -0400

    WIP fix typo and add checkout back

--- rakefile.rb
+++ rakefile.rb
@@ -5,9 +5,11 @@ desc 'publish site : merge master to gh-pages and push to github'
 task :publish do
   working_dir = File.dirname(__FILE__)
   git = Git.open(working_dir, log: Logger.new(STDOUT))
+  current_branch = git.branches.local
   git.checkout('gh-pages')
   git.merge('master')
-  g.push(g.remote('origin'))
+  git.push(g.remote('origin'))
+  git.checkout(current_branch)
 end
 
 desc 'list all task'
